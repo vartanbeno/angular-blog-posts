975d117fa8aaa2b548b901e54f39a895d5c17416
vartanbeno <vartanbeno@gmail.com>
2018-07-09 15:47:53 -0400

    implement getAllPosts function in show-post component

--- src/app/show-post/show-post.component.ts
+++ src/app/show-post/show-post.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ShowPostService } from '../show-post.service';
 
 @Component({
   selector: 'app-show-post',
   templateUrl: './show-post.component.html',
-  styleUrls: ['./show-post.component.css']
+  styleUrls: ['./show-post.component.css'],
+  providers: [ ShowPostService ]
 })
 export class ShowPostComponent implements OnInit {
 
-  constructor() { }
+  public posts: any = [];
+
+  constructor(private showPostService: ShowPostService) { }
 
   ngOnInit() {
+    this.getAllPosts();
+  }
+
+  getAllPosts() {
+    this.showPostService.getAllPosts().subscribe(result => {
+      this.posts = result['data'];
+    })
   }
 
 }

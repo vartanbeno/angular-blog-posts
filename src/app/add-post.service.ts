da63ac6ac19ead01431c49163d8b700c41ee29ee
vartanbeno <vartanbeno@gmail.com>
2018-07-09 17:03:50 -0400

    create add post service and implement adding functionality

---
+++ src/app/add-post.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Post } from './models/post.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddPostService {
+
+  constructor(private http: HttpClient) { }
+
+  addPost(post: Post) {
+    return this.http.post('api/post/createPost', {
+      title: post.title,
+      description: post.description
+    })
+  }
+
+}
--- src/app/add-post/add-post.component.ts
+++ src/app/add-post/add-post.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from '../models/post.model';
+import { AddPostService } from '../add-post.service';
 
 @Component({
   selector: 'app-add-post',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPostComponent implements OnInit {
 
-  constructor() { }
+  public post: Post;
+
+  constructor(private addPostService: AddPostService) { }
 
   ngOnInit() {
+  }
+
+  addPost() {
+    if (this.post.title && this.post.description) {
+      this.addPostService.addPost(this.post).subscribe(res => {
+        console.log('result is ', res);
+      })
+    }
+    else {
+      alert('Both title and description are required.');
+    }
   }
 
 }

4802005bbcbf5ef8dd5fea620a4d1abde3cfec20
Vartan Benohanian <vartanbeno@gmail.com>
2018-07-08 20:10:21 -0400

    set up mongodb and proxy

--- server/app.js
+++ server/app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const url = 'mongodb://localhost/blogDb';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/api/user/login', (req, res) => {
-    res.send('Hello world');
+    mongoose.connect(url, err => {
+        if (err) throw err;
+        console.log('connection successful, username is ' + req.body.username + ', password is ' + req.body.password);
+    })
 })
 
 app.listen(3000, () => console.log('blog server running on port 3000'));
